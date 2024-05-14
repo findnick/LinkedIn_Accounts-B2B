77425017e63ed0af4ddab8f19950e84fe7915e8e
Nabeel Imtiaz <hitmanboy27@gmail.com>
2024-05-14 23:06:54 +0500

    changes

--- src/Apis/usePostApi.jsx
+++ src/Apis/usePostApi.jsx
@@ -7,7 +7,7 @@ function usePostApi(path) {
 
   const fetch = async (data,header=null) => {
     try {
-      const response = await axios.post(`https://linkedbackendinfinal.vercel.app/api/${path}`, data, {headers:header})
+      const response = await axios.post(`https://linkedbackendinfinal.vercel.app/https/access-control-allow-origin/api/${path}`, data, {headers:header})
       console.log(response);
       return response
     } catch (error) {
